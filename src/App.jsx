b269ff9e9a4e04d9d3a4840c20bccbdfba321517
bhagyesh Patil <bhagyesh02patil02@gmail.com>
2025-07-28 18:23:47 +0530

    Fix: Added Vercel SpeedInsights and cleaned up App component

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Projects from "./sections/Projects";
 import Experiences from "./sections/Experiences";
 import Contact from "./sections/Contact";
 import Footer from './sections/Footer';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights";
 const App = () => {
   return (
     <div className="container mx-auto max-w-7xl">
